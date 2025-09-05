676e575888ff93ce8f9254cf0cc9e3f05c7680e3
gomesjosias.1976 <gomesjosias.1976@gmail.com>
2025-09-05 21:31:06 +0000

    commit6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myapp/firebase_options.dart';
+import 'package:myapp/src/view/cadastro_clientes.dart';
 import 'package:myapp/src/view/login_screen.dart';
 
 
@@ -12,15 +13,13 @@ void main() async {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: /*LoginScreen(),*/ CadastroClientes(),
     );
   }
 }
