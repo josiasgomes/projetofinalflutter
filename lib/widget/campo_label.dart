e502062d31f31fe9aa756163d855bfd9b4b982bb
gomesjosias.1976 <gomesjosias.1976@gmail.com>
2025-09-05 22:28:10 +0000

    commit7

---
+++ lib/widget/campo_label.dart
@@ -0,0 +1,52 @@
+// lib/campo_texto_personalizado.dart
+import 'package:flutter/material.dart';
+
+class CampoLabel extends StatelessWidget {
+  final String label;
+  final String placeholder;
+  
+  const CampoLabel({
+    Key? key,
+    required this.label,
+    required this.placeholder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start, // Alinha o texto à esquerda
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Roboto'
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          width: 330,
+          decoration: BoxDecoration(
+            color: Color(0xffFFFFFF),
+            boxShadow: [
+              BoxShadow(color: Color.black.withOpacity(0.1), 
+              blurRadius: 10)
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              labelText: placeholder,
+              labelStyle: const TextStyle(
+                color: Color(0xffC0C0C0),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
