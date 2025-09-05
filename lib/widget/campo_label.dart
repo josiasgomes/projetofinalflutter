0c1a7ed971193f7f6745ac3a63ffc8f5b4318460
gomesjosias.1976 <gomesjosias.1976@gmail.com>
2025-09-05 22:39:17 +0000

    label_cadastro

--- lib/widget/campo_label.dart
+++ lib/widget/campo_label.dart
@@ -4,12 +4,9 @@ import 'package:flutter/material.dart';
 class CampoLabel extends StatelessWidget {
   final String label;
   final String placeholder;
-  
-  const CampoLabel({
-    Key? key,
-    required this.label,
-    required this.placeholder,
-  }) : super(key: key);
+
+  const CampoLabel({Key? key, required this.label, required this.placeholder})
+    : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +18,7 @@ class CampoLabel extends StatelessWidget {
           style: const TextStyle(
             fontSize: 14,
             fontWeight: FontWeight.bold,
-            fontFamily: 'Roboto'
+            fontFamily: 'Roboto',
           ),
         ),
         const SizedBox(height: 8),
@@ -30,9 +27,12 @@ class CampoLabel extends StatelessWidget {
           decoration: BoxDecoration(
             color: Color(0xffFFFFFF),
             boxShadow: [
-              BoxShadow(color: Color.black.withOpacity(0.1), 
-              blurRadius: 10)
-            ]
+              BoxShadow(
+                color: Colors.black,
+                offset: Offset(0, 8),
+                blurRadius: 10,
+              ),
+            ],
           ),
           child: TextField(
             decoration: InputDecoration(
@@ -40,9 +40,7 @@ class CampoLabel extends StatelessWidget {
                 borderRadius: BorderRadius.circular(30.0),
               ),
               labelText: placeholder,
-              labelStyle: const TextStyle(
-                color: Color(0xffC0C0C0),
-              ),
+              labelStyle: const TextStyle(color: Color(0xffC0C0C0)),
             ),
           ),
         ),
