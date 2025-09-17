31c4896908f16ef1da1f0910d8f3c9fa6c5eaa05
albertomallet100 <albertomallet100@gmail.com>
2025-09-17 22:17:04 +0000

    adicionou nav_bar

--- lib/src/view/clients_screen.dart/clients_screen.dart
+++ lib/src/view/clients_screen.dart/clients_screen.dart
@@ -1,35 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/widget/barra_navegacao_principal.dart';
+import 'package:myapp/widget/nav_bar.dart';
 
 class ClientsListScreen extends StatelessWidget {
   const ClientsListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Clientes Cadastrados'),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            // Volta para a tela anterior
-            Navigator.pop(context);
-          },
-        ),
-      ),
+    return NavBar(
+      title: 'Clientes Cadastrados',
       body: ListView.builder(
         itemCount: 6, // Este número será dinâmico, vindo do banco de dados
         itemBuilder: (context, index) {
           return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
             child: Card(
               elevation: 4,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(15.0),
               ),
               child: ListTile(
-                contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
                 title: Text(
                   'Cliente ${index + 1}', // Este texto será o nome do cliente
                   style: const TextStyle(
@@ -46,18 +42,6 @@ class ClientsListScreen extends StatelessWidget {
           );
         },
       ),
-      // Botão de Ação Flutuante centralizado
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Ação do botão de adicionar
-        },
-        backgroundColor: Colors.blue.shade800,
-        elevation: 8.0,
-        child: const Icon(Icons.add, size: 35),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      // Nova barra de navegação
-      bottomNavigationBar: const BarraNavegacaoPrincipal(),
     );
   }
 }
---
+++ lib/widget/nav_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/src/view/cadastro_clientes.dart';
+import 'package:myapp/widget/barra_navegacao_principal.dart';
+
+class NavBar extends StatelessWidget {
+  final Widget body;
+  final String title;
+
+  const NavBar({super.key, required this.body, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: body,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const CadastroClientes()),
+          );
+        },
+        backgroundColor: const Color(0xFF0D47A1), // Azul escuro para o botão
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BarraNavegacaoPrincipal(
+        onHomePressed: () {
+          // Lógica para ir para Home
+        },
+        onListPressed: () {
+          // Lógica para ir para Orçamentos
+        },
+        onNotificationsPressed: () {
+          // Lógica para ir para Notificações
+        },
+        onSettingsPressed: () {
+          // Lógica para ir para Configurações
+        },
+      ),
+    );
+  }
+}
