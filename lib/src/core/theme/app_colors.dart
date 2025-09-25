85e2df8dd9789cf727313f73c49f9b463c9d045e
isac3299 <isac3299@gmail.com>
2025-09-25 22:53:55 +0000

    [update]: Criação do tema do aplicativo.

---
+++ lib/src/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color.fromRGBO(174, 17, 188, 1);
+  static const Color secundary = Color.fromRGBO(70, 85, 255, 1);
+  static const Color tertiary = Color.fromRGBO(6, 64, 175, 1);
+  static const Color buttonBg = Color.fromRGBO(196, 196, 196, 1);
+  static const Color background = Colors.white;
+  static const Color text = Colors.black;
+}
---
+++ lib/src/core/theme/app_textStyle.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/src/core/theme/app_colors.dart';
+
+class AppTextstyle {
+  //heading and subtitles
+  static TextStyle heading1 = TextStyle(
+    fontSize: 30,
+    fontFamily: "Otomanopee One",
+    color: AppColors.secundary,
+  );
+
+  static TextStyle heading2 = TextStyle(
+    fontSize: 25,
+    fontFamily: "Montserrat",
+    fontWeight: FontWeight.bold,
+    color: AppColors.secundary,
+  );
+
+  static TextStyle subTitle1 = TextStyle(
+    fontSize: 20,
+    fontFamily: "Montserrat",
+    color: AppColors.primary,
+  );
+
+  //texts sizes
+  static TextStyle textSmall1 = TextStyle(
+    fontSize: 12,
+    fontFamily: "Montserrat",
+    color: AppColors.text,
+  );
+
+  static TextStyle textSmall2 = TextStyle(
+    fontSize: 12,
+    fontFamily: "Roboto",
+    color: AppColors.text,
+  );
+
+  static TextStyle textLarge1 = TextStyle(
+    fontSize: 20,
+    fontFamily: "Montserrat",
+    color: AppColors.text,
+  );
+
+  static TextStyle textLarge2 = TextStyle(
+    fontSize: 20,
+    fontFamily: "Roboto",
+    color: AppColors.text,
+  );
+}
