9fcc70c6633b39154ca22cc5ab2d328d52f8f8d7
danielalexandre.bios <danielalexandre.bios@gmail.com>
2025-10-01 00:29:22 +0000

    enviando o que faltava  lça

---
+++ lib/src/view/full_screen_relatorios.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FullScreenChartScreen extends StatelessWidget {
+  final String title;
+  final Widget chart;
+
+  const FullScreenChartScreen({super.key, required this.title, required this.chart});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: chart,
+        ),
+      ),
+    );
+  }
